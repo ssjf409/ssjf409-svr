747de0ed03a36f25fb150b2d4205bbad9849b1e2
ssjf409 <35087350+ssjf409@users.noreply.github.com>
2018-01-11 12:14:25 +0900

    cc

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ var signalClients = [];
 var rooms = {};
 
 app.set( 'port', process.env.PORT || 3009 );
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
+  
 app.get('/', function(req, res, next){
   res.send('Hello World');  
 });
